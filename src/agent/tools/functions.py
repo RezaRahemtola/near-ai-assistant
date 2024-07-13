2dd6295b04d13897f7859b95165a002a9f76801b
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-07-13 09:32:02 +0200

    wip: Tools call generated

--- src/agent/tools/functions.py
+++ src/agent/tools/functions.py
@@ -1,3 +1,12 @@
+from langchain.tools import tool
+from langchain_core.utils.function_calling import convert_to_openai_tool
+
+@tool()
+def search_google(message: str) -> str:
+    """Search on Google"""
+    print("here")
+    return "fake"
+
 def get_tools() -> list:
-    # TODO
-    return []
+    tools = [search_google]
+    return [convert_to_openai_tool(t) for t in tools]
